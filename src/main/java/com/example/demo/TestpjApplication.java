89b6e1536b24a1dbf43140abc925c0b9cc17abf0
jinbong seo <jbfamily00@gmail.com>
2017-09-22 11:42:23 +0900

    1

--- src/main/java/com/example/demo/TestpjApplication.java
+++ src/main/java/com/example/demo/TestpjApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestpjApplication {
  
+	//시작
 	public static void main(String[] args) {
 		SpringApplication.run(TestpjApplication.class, args);
 	}
